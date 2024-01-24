25fe5de18179887b7b21804f0792e606d6a1f3fe
John Mark <j0hnm4rkcuyos@gmail.com>
2024-01-24 20:19:12 +0800

    update

--- index.php
+++ index.php
@@ -1,25 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home Page</title>
 </head>
+
 <body>
     <!-- <form action="controller/add.php" method="post"> -->
-        <div>
-            <label for="name">Enter name:</label>
-            <input type="text" id="name" name="name">
-        </div>
-        <div>
-            <label for="email">Enter email:</label>
-            <input type="email" id="email" name="email">
-        </div>
-        <div>
-            <label for="password">Enter password:</label>
-            <input type="password" id="password" name="password">
-        </div>
-        <button onclick="add()">Submit</button>
+    <div>
+        <label for="name">Enter name:</label>
+        <input type="text" id="name" name="name">
+    </div>
+    <div>
+        <label for="email">Enter email:</label>
+        <input type="email" id="email" name="email">
+    </div>
+    <div>
+        <label for="password">Enter password:</label>
+        <input type="password" id="password" name="password">
+    </div>
+    <button onclick="add()">Submit</button>
     <!-- </form> -->
     <table>
         <tr>
@@ -27,26 +29,27 @@
             <th>Name</th>
             <th>Email</th>
             <th>Actions</th>
-        </tr> 
-        <?php 
-           require('controller/config.php');
-           $select="select * from users";
-           $res=mysqli_query($conn,$select);
-           while($row=mysqli_fetch_assoc($res)){ ?>
-                    <tr>
-                        <td><?php echo $row['id'];?></td> 
-                        <td><?php echo $row['name'];?></td>   
-                        <td><?php echo $row['email'];?></td> 
-                        <td>
-                            <button onclick="update('<?php echo $row['id'];?>')">Update</button>
-                            <button onclick="deleteme('<?php echo $row['id'];?>')">Delete</button>
-                        </td> 
-                    </tr> 
-        <?php  } 
+        </tr>
+        <?php
+        require('controller/config.php');
+        $select = "select * from users";
+        $res = mysqli_query($conn, $select);
+        while ($row = mysqli_fetch_assoc($res)) { ?>
+            <tr>
+                <td><?php echo $row['id']; ?></td>
+                <td><?php echo $row['name']; ?></td>
+                <td><?php echo $row['email']; ?></td>
+                <td>
+                    <button onclick="update('<?php echo $row['id']; ?>')">Update</button>
+                    <button onclick="deleteme('<?php echo $row['id']; ?>')">Delete</button>
+                </td>
+            </tr>
+        <?php  }
         ?>
-         
+
     </table>
-<script src="js/jquery.js"></script>
-<script src="js/main.js"></script>
+    <script src="js/jquery.js"></script>
+    <script src="js/main.js"></script>
 </body>
+
 </html>
